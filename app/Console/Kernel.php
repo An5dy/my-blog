891da2577378566fc213fd936fc01f04559984ee
ze <ze@bluereader.cn>
2018-04-14 00:23:02 +0800

    新增清除opcahce指令

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\ServiceMakeCommand::class,
-        \App\Console\Commands\ResetOpcache::class,
     ];
 
     /**
